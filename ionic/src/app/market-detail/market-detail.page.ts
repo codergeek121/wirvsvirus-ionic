943944d3044859e8095c092f054ff67e802b08cc
Niklas Häusele <niklas.haeusele@gmail.com>
2020-03-21 14:51:18 +0100

    timeslotppicker

--- ionic/src/app/market-detail/market-detail.page.ts
+++ ionic/src/app/market-detail/market-detail.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { PickerController } from '@ionic/angular';
 
 @Component({
   selector: 'app-market-detail',
@@ -7,15 +8,61 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./market-detail.page.scss'],
 })
 export class MarketDetailPage implements OnInit {
+  market = {
+    name: "Testmarkt abc",
+    street: "Musterweg 13",
+    plz: "80801",
+    city: "München"
+  }
+
+  timeslots = [
+    { id: 1, starts_at: "12:00", ends_at: "12:20" },
+    { id: 2, starts_at: "12:20", ends_at: "12:40" },
+    { id: 3, starts_at: "12:40", ends_at: "12:60" },
+    { id: 4, starts_at: "13:00", ends_at: "13:20" }
+  ]
+
+  selectedTimeslot = {
+    id: 1,
+    desc: ""
+  }
 
   constructor(
     private router: Router,
-    private activeRoute: ActivatedRoute
+    private activeRoute: ActivatedRoute,
+    private pickerController: PickerController
   ) { }
 
   ngOnInit() {
     this.activeRoute.params.subscribe(params => {
       console.log(params["id"])
+    })
+  }
+
+  public async openPicker() {
+    const picker = await this.pickerController.create({
+      columns: [
+        { name: "timeslots", options: this.timeSlotsToColumns()}
+      ],
+      buttons: [
+        { text: 'Cancel', role: 'cancel' },
+        { text: 'Confirm', handler: (value) => {
+          console.log(value)
+          this.selectedTimeslot.id = value.timeslots.value
+          this.selectedTimeslot.desc = value.timeslots.text
+        }},
+      ]
+    })
+    await picker.present();
+  }
+
+  public bookTimeslot() {
+    console.log("Timeslot gebucht:", this.selectedTimeslot)
+  }
+
+  private timeSlotsToColumns() {
+    return this.timeslots.map( slot => { 
+      return { text: `${slot.starts_at} bis ${slot.ends_at}`, value: slot.id} 
     })
   }
 }
