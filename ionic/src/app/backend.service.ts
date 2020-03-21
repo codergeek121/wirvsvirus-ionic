9aab436f123d50b7824e28a00f6c3e81a13af5ab
SvenReichersdörfer <sven.reichersdoerfer@uni-ulm.de>
2020-03-21 13:11:37 +0100

    added freeSlots/bookSlot/validateBookingCode apis

--- ionic/src/app/backend.service.ts
+++ ionic/src/app/backend.service.ts
@@ -1,16 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HomePage } from './home/home.page';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
 
-  constructor(private http: HttpClient) {}
-
-  public searchMarkets(postalCode, typeOfMarket) {
+	constructor(private http: HttpClient) {}
+	
+  public searchMarkets(postalCode : String, typeOfMarket : String) {
     const url = `/assets/json/markets.json?plz=${postalCode}&type_of_market=${typeOfMarket}`;
-    const markets = this.http.get(url);
-    return markets;
-  }
+    return this.http.get(url);
+    
+	}
+	
+	public getFreeSlots(marketId : number, dateString : String) {
+    const url = `/assets/json/freeSlots.json?id=${marketId}&date=${dateString}`;
+    return this.http.get(url);
+	}
+	
+	public bookSlot(slot_id : number) {
+    const url = `/assets/json/bookSlot.json`;
+    return this.http.post(url, {id: slot_id});
+	}
+	
+  public validateBookingCode(bookingCode : String) {
+    const url = `/assets/json/validateBookingCode.json`;
+    return this.http.post(url, {booking_code: bookingCode});
+	}
+	
 }
