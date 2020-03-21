9f981b3289cc170aa5fca386e710bd2e9343d7f3
SvenReichersdörfer <sven.reichersdoerfer@uni-ulm.de>
2020-03-21 15:47:20 +0100

    added restful apis

--- ionic/src/app/backend.service.ts
+++ ionic/src/app/backend.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 import { HomePage } from './home/home.page';
 
 @Injectable({
@@ -7,27 +8,40 @@ import { HomePage } from './home/home.page';
 })
 export class BackendService {
 
+	authurl = "https://backend.geplant-einkaufen.de/api-auth/login"
+
+	//hostUrl = 'https://backend.geplant-einkaufen.de/' //add host url
+	hostUrl = '' //add host url
+	marketUrl = this.hostUrl +'/assets/json/markets.json'
+	//marketUrl = this.hostUrl +'stores'
+	
 	constructor(private http: HttpClient) {}
 	
   public searchMarkets(postalCode : String, typeOfMarket : String) {
-    const url = `/assets/json/markets.json?plz=${postalCode}&type_of_market=${typeOfMarket}`;
+
+    const url = this.marketUrl + `?plz=${postalCode}&type_of_market=${typeOfMarket}`;
     return this.http.get(url);
-    
+
 	}
 	
 	public getFreeSlots(marketId : number, dateString : String) {
-    const url = `/assets/json/freeSlots.json?id=${marketId}&date=${dateString}`;
+		const url = `/assets/json/freeSlots.json?id=${marketId}&date=${dateString}`;
+    //const url = this.marketUrl + `/${marketId}/freeSlots&date=${dateString}`;
     return this.http.get(url);
 	}
 	
-	public bookSlot(slot_id : number) {
-    const url = `/assets/json/bookSlot.json`;
-    return this.http.post(url, {id: slot_id});
+	public bookSlot(marketId: number, slot_id : number) {
+		const url = `/assets/json/bookSlot.json`;
+    //const url = this.marketUrl + `/${marketId}/slots/${slot_id}`;
+    //return this.http.post(url, {id: slot_id});
+    return this.http.get(url);
 	}
 	
-  public validateBookingCode(bookingCode : String) {
-    const url = `/assets/json/validateBookingCode.json`;
-    return this.http.post(url, {booking_code: bookingCode});
+  public validateBookingCode(marketId: number, slotId: number, bookingCode : String) {
+	  const url = `/assets/json/validateBookingCode.json`;
+    //const url = this.marketUrl + `/${marketId}/slots/${slotId}/code`;
+    //return this.http.post(url, {booking_code: bookingCode});
+    return this.http.get(url);
 	}
 	
 }
