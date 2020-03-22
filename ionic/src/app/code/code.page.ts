47ecaacdc648e7b7d4e7c6828ecc1e9974375782
Niklas Häusele <niklas.haeusele@gmail.com>
2020-03-22 13:47:13 +0100

    Add Store to code page

--- ionic/src/app/code/code.page.ts
+++ ionic/src/app/code/code.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { StoreCapacity } from '../services/backend.service';
+import { StoreCapacity, Store } from '../services/backend.service';
 
 @Component({
   selector: 'app-code',
@@ -10,6 +10,7 @@ import { StoreCapacity } from '../services/backend.service';
 export class CodePage implements OnInit {
   public booking_code: number;
   public store_capacity: StoreCapacity;
+  public store: Store;
 
   constructor(
     private router: Router
@@ -17,6 +18,7 @@ export class CodePage implements OnInit {
     const params = <any>this.router.getCurrentNavigation().extras.state
     console.log(params)
     this.booking_code = params.identifier
+    this.store = <Store>params.store_capacity.store
     this.store_capacity = <StoreCapacity>params.store_capacity
   }
 
