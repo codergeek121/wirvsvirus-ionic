f2a7ff0b14913a4e2ee8a2e70f1f97c39a064524
Niklas Häusele <niklas.haeusele@gmail.com>
2020-03-22 13:25:47 +0100

    adds store card

--- ionic/src/app/components/store/store.component.ts
+++ ionic/src/app/components/store/store.component.ts
@@ -1,17 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Store } from 'src/app/services/backend.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
   styleUrls: ['./store.component.scss'],
 })
-export class StoreComponent implements OnInit {
-  @Input() store: Observable<Store>;
+export class StoreComponent {
+  @Input() store: Store;
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  constructor() {}
 }
