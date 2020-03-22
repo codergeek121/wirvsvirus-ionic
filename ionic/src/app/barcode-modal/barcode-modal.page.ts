5e913d56318b9b5db261f8d59fe017cd64d0669a
Niklas Häusele <niklas.haeusele@gmail.com>
2020-03-22 18:43:32 +0100

    Shows a list of codes in modal

--- ionic/src/app/barcode-modal/barcode-modal.page.ts
+++ ionic/src/app/barcode-modal/barcode-modal.page.ts
@@ -7,6 +7,7 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./barcode-modal.page.scss'],
 })
 export class BarcodeModalPage implements OnInit {
+  codes = ["abcd", "efghi"]
 
   constructor(
     private modalController: ModalController
